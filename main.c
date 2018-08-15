ea1367e36432c2ebe9d983eeecd23e2a5a70a825
Taha Azhar <taha-azhar@hotmail.com>
2018-08-15 17:39:02 +0500

    The initial program that swaps two numbers

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x = 8;
+    int y = 9;
+
+    printf("x is %i\n", x);
+    printf("y is %i\n",y);
+
+    // swapping
+
+    printf("x is now %i\n", x);
+    printf("y is now %i\n", y);
+}
